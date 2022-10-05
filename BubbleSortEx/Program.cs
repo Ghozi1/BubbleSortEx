b47d65361c2af4b287acf557d89577f8386f5081
Ghozi1 <mghozighifar04@gmail.com>
2022-10-05 20:22:31 +0700

    Membuat method bubblesort yang didalamnya terdapat function for perbandingan element pertama dengan element selanjutnya

--- BubbleSortEx/Program.cs
+++ BubbleSortEx/Program.cs
@@ -57,6 +57,26 @@ namespace BubbleSortEx
         }
 
 
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) //For n-1 passes
+            {
+                // Pada pass i, bandingkan n-i element pertama dengan elemen selanjutnya 
+                for (int j =0; j < n; j++)
+                {
+                    if (a[j] > a[j+ 1]) // Jika elemen tidak diatur dalam urutan yang benar 
+                    {
+                        // Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
 
     }
 }
