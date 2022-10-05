da8b98dff04545c0431099fa21a5478af887a6b9
Ghozi1 <mghozighifar04@gmail.com>
2022-10-05 20:04:46 +0700

    Membuat sebuah kelas program yang mana didalamnya terdapat pendeklarasian sebuah private int

--- BubbleSortEx/Program.cs
+++ BubbleSortEx/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace BubbleSortEx
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
-        {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
 
+        // Deklarasi variable int untuk menyimpan banyaknya data pada array 
+        private int n;
+
+        // Fungsi /  methood untuk menerima masukan     
+        public void read()
+        {
+            // Menerima angka untuk mementukan banyaknya data yang disimpan pada array 
+            while (true)
+            {
+                Console.WriteLine("Masukkan banyaknya element pada array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 element. \n");
+            }
         }
+
     }
 }
